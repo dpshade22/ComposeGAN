ec48b45ed56b019b75c9728c51832eca77676ece
Dylan Shade <63427984+dpshade22@users.noreply.github.com>
2023-02-23 21:39:14 -0500

    new model and more bad data

--- src/midi.py
+++ src/midi.py
@@ -5,8 +5,8 @@ from model import predictCSV, saveMidiFromCSV
 model = load_model("models/gru/79onBigData/model.h5")
 
 # Define the range of values for each column in the CSV file
-trackRange = range(1, 3)
-noteRange = range(0, 127)
+trackRange = range(1, 2)
+noteRange = range(21, 127)
 velocityRange = range(0, 127)
 timeRange = range(0, 240)
 
@@ -37,27 +37,49 @@ def gradedPopulation(population):
     return [x[1] for x in sorted(population, reverse=True)]
 
 
-def mutateBest(bestIndividual, populationSize=100, numMutations=3):
+def mutateBest(bestIndividual, populationSize=100, numMutations=3, copyProb=0.15, transProb=0.15):
     mutatedPop = [bestIndividual]
     for x in range(populationSize):
         individualCopy = bestIndividual.copy()
 
         for _ in range(numMutations):
-            row_to_mutate = random.randint(0, len(bestIndividual) - 1)
-            col_to_mutate = random.choice(["track", "note", "velocity", "time"])
+            if random.random() < transProb:
+                # Transpose a random group of MIDI messages
+                group_size = random.randint(1, 4)
+                start_index = random.randint(0, len(bestIndividual) - group_size)
+                delta = random.randint(-6, 6)
 
-            current_val = individualCopy.at[row_to_mutate, col_to_mutate]
-            delta_val = random.randint(-12, 12)
-            new_val = min(
-                max(current_val + delta_val, eval(f"{col_to_mutate}Range").start),
-                eval(f"{col_to_mutate}Range").stop - 1,
-            )
+                for i in range(start_index, start_index + group_size):
+                    for col in ["note", "velocity"]:
+                        current_val = individualCopy.at[i, col]
+                        new_val = min(
+                            max(current_val + delta, eval(f"{col}Range").start),
+                            eval(f"{col}Range").stop - 1,
+                        )
+                        individualCopy.at[i, col] = new_val
+            if random.random() < copyProb:
+                # Copy a random MIDI message from the best individual
+                row_to_copy = random.randint(0, len(bestIndividual) - 1)
+                individualCopy.iloc[row_to_copy] = bestIndividual.iloc[row_to_copy]
 
-            individualCopy.at[row_to_mutate, col_to_mutate] = new_val
+            else:
+                # Mutate a random MIDI message in the individual copy
+                row_to_mutate = random.randint(0, len(bestIndividual) - 1)
+                col_to_mutate = random.choice(["track", "note", "velocity", "time"])
+
+                current_val = individualCopy.at[row_to_mutate, col_to_mutate]
+                delta_val = random.randint(-12, 12)
+                new_val = min(
+                    max(current_val + delta_val, eval(f"{col_to_mutate}Range").start),
+                    eval(f"{col_to_mutate}Range").stop - 1,
+                )
+
+                individualCopy.at[row_to_mutate, col_to_mutate] = new_val
 
         mutatedPop.append(individualCopy)
 
     return mutatedPop
+
 
 
 def evolve():
